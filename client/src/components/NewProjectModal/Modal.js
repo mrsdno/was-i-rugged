f026bc9db3039017619d986b712be98eef37ffd5
mrsdno <christine@dno.dev>
2022-07-31 12:06:09 -0400

    show project on front end after adding through modal

--- client/src/components/NewProjectModal/Modal.js
+++ client/src/components/NewProjectModal/Modal.js
@@ -2,46 +2,54 @@ import "./Modal.css";
 import React, { useState } from "react";
 import { ADD_PROJECT } from "../../utils/mutations";
 import { useMutation } from "@apollo/client";
+import { QUERY_PROJECTS } from "../../utils/queries";
 
 
 const Modal = ({ handleClose, show, children }) => {
-const showHideClassName = show ? "modal display-block" : "modal display-none";
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
 
-    const [name, setNewProjectName] = useState("");
-    const [twitter, setNewProjectTwitter] = useState("");
-    const [discord, setNewProjectDiscord] = useState("");
-    const [addProject, { data, loading, error }] = useMutation(ADD_PROJECT);
-    const picture = "this is the picture";
-    const status = "active";
+  const [name, setNewProjectName] = useState("");
+  const [twitter, setNewProjectTwitter] = useState("");
+  const [discord, setNewProjectDiscord] = useState("");
+  const [addProject, { data, loading, error }] = useMutation(ADD_PROJECT, {
+    refetchQueries: [
+      { query: QUERY_PROJECTS }, 
+      "projects"
+    ]
+  });
 
-    const handleInputChange = (e) => {
-      // Getting the value and name of the input which triggered the change
-      const { target } = e;
-      const inputType = target.name;
-      const inputValue = target.value;
 
-      // Based on the input type, we set the state of either email, username, and password
-      if (inputType === "name") {
-        setNewProjectName(inputValue);
-      } else if (inputType === "discord") {
-        setNewProjectDiscord(inputValue);
-      } else {
-        setNewProjectTwitter(inputValue);
-      }
-    };
+  const picture = "this is the picture";
+  const status = "active";
 
-    const handleSubmit = async (event) => {
-      event.preventDefault();
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
 
-      try {
-        // add thought to database
-        await addProject({
-          variables: { name, twitter, discord, picture, status },
-        });
-      } catch (e) {
-        console.error(e);
-      }
-    };
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === "name") {
+      setNewProjectName(inputValue);
+    } else if (inputType === "discord") {
+      setNewProjectDiscord(inputValue);
+    } else {
+      setNewProjectTwitter(inputValue);
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      // add thought to database
+      await addProject({
+        variables: { name, twitter, discord, picture, status },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <div className={showHideClassName}>
@@ -71,7 +79,9 @@ const showHideClassName = show ? "modal display-block" : "modal display-none";
             placeholder="Project Twitter"
           />
           {error && <span className="ml-2">Something went wrong...</span>}
-          <button type="submit" onClick={handleClose}>Submit</button>
+          <button type="submit" onClick={handleClose}>
+            Submit
+          </button>
         </form>
 
         <button type="button" onClick={handleClose}>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,14 @@
 import { useQuery } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
 import { QUERY_PROJECTS } from "../utils/queries";
 import Projects from '../components/ProjectsList'
 
 const Home = () => {
+
   const { loading, data } = useQuery(QUERY_PROJECTS);
   const projects = data?.projects || [];
+
+
   return (
     <div className="banner">
           <div className="col-12 col-md-8 mb-3">
